b78e8d5a37806602cd9f395d67f93b02392f33c6
DhananjayB <dhbiawat21@gmail.com>
2023-05-10 15:54:12 +0530

    first commit

---
+++ tictac/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Board from './Components/Board';
+//import Square from './Components/Square';
+
+function App() {
+  return (
+    <>
+  <Board /> 
+ {/* <Square />*/}
+ </>
+  );
+}
+
+export default App;
---
+++ tictac/src/Components/Square.js
@@ -0,0 +1,19 @@
+import React from 'react'
+//import { useState } from 'react'
+
+const Square = ({value, onSquareClick}) => {
+
+    //const [value, setValue] = useState(null) ;
+
+   // const handleClick = () => {
+        //console.log("Clicked !")
+       // setValue('X')
+    //}
+  return (
+    <>
+    <button className='square' onClick={onSquareClick}> {value} </button>
+    </>
+  )
+}
+
+export default Square
